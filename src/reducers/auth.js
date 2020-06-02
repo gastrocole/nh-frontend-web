613405bc522f0ecb925958614a960b2a585ccb12
Haidee <haideeob@hotmail.co.uk>
2020-06-02 15:50:00 +0100

    added reducers/auth and types/reducerTypes

---
+++ src/reducers/auth.js
@@ -0,0 +1,52 @@
+import {
+    USER_LOADED,
+    AUTH_ERROR,
+    REGISTER_SUCCESS,
+    REGISTER_FAIL,
+    LOGIN_SUCCESS,
+    LOGIN_FAIL,
+    CLEAR_PROFILE, 
+    LOGOUT
+} from '../types/reducerTypes'
+
+const initialState = {
+    token = localStorage.getItem('token'),
+    isAuthenticated = null, 
+    loading = true,
+    user: null
+}
+
+export default function (state = initialState, action) {
+    const { type, payload } = action
+    switch (type) {
+        case USER_LOADED:
+            return {
+                ...state,
+                isAuthenticated: true,
+                loading: false,
+                user: payload
+            };
+        case LOGIN_SUCCESS:
+        case REGISTER_SUCCESS:
+            return {
+                ...state,
+                ...payload,
+                isAuthenticated: true,
+                loading: false
+            };
+        case AUTH_ERROR:
+        case REGISTER_FAIL:
+        case LOGIN_FAIL:
+        case CLEAR_PROFILE:
+        case LOGOUT:
+            return {
+                ...state,
+                token: null,
+                isAuthenticated: false, 
+                loading: false,
+            };
+        default:
+            return state;
+    }
+    
+}
