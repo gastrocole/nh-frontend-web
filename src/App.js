5eb3711a950bb40658bc6b0fd0cdd555bb177522
Cole O'Brien <gastrocole@gmail.com>
2020-06-08 13:46:44 +0100

    Setup pages and page routing

--- src/App.js
+++ src/App.js
@@ -1,36 +1,52 @@
+// Standard imports: React, Router & Redux
 import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
+// Custom components
 import ResponsiveContainer from './components/layout/ResponsiveContainer';
 import AuthRoute from './components/routing/AuthRoute';
-import LandingPage from './components/landing/LandingPage';
-import HomePage from './components/home/HomePage';
 
-import store from './store';
+// Pages
+import Home from './pages/Home';
+import Membership from './pages/Membership';
+import Partners from './pages/Partners';
+import Dashboard from './pages/Dashboard';
+import NotFound from './pages/NotFound';
 
-import setAuthToken from './utils/auth';
-import { loadUser } from './actions/auth';
-
+// Styles
 import './App.css';
 
+// Redux Store
+import store from './store';
+
+// Actions
+import setAuthToken from './utils/auth';
+
+// Utility functions
+import { loadUser } from './actions/auth';
+
+// Set api auth token from local storage
 if (localStorage.token) {
   setAuthToken(localStorage.token);
 }
 
 const App = () => {
   useEffect(() => {
+    // Attempt to load user when app starts
     store.dispatch(loadUser());
   }, []);
   return (
     <Provider store={store}>
       <Router>
         <Fragment className='App'>
-          {/*insert nav bar here*/}
           <ResponsiveContainer>
             <Switch>
-              <Route exact path='/' component={LandingPage} />
-              <AuthRoute exact path='/home' component={HomePage} />
+              <Route exact path='/' component={Home} />
+              <Route exact path='/membership' component={Membership} />
+              <Route exact path='/partners' component={Partners} />
+              <AuthRoute exact path='/dashboard' component={Dashboard} />
+              <Route component={NotFound} />
             </Switch>
           </ResponsiveContainer>
         </Fragment>
